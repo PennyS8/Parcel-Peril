fbc84124ebeaee6713d4c43de21662bbad5a8910
PennyS8 <silliman25@up.edu>
2023-06-22 04:15:31 -0700

    Silliman__created basic level and Tiled importing

--- scripts/level.py
+++ scripts/level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from pygame import image
 
 class Level:
     def __init__(self):
@@ -18,14 +20,28 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visable_sprites, self.obstacles_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visable_sprites], self.obstacles_sprites)
+        layout = {
+            'boundry': import_csv_layout('graphics/maps/conveyor_hell/Boundry.csv'),
+            'block': import_csv_layout('graphics/maps/conveyor_hell/Blocks.csv')
+        }
+        graphics = {
+            'block': import_folder('graphics/maps/conveyor_hell_tileset')
+        }
+
+        for style, layout in layout.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+
+                    if style == 'boundry':
+                        Tile((x, y), [self.obstacles_sprites], 'invisible')
+                    if style == 'block':
+                        surf = graphics['block'][int(col)]
+                        Tile((x, y), [self.obstacles_sprites, self.visable_sprites], 'block', surf)
+                        
+        self.player = Player((6 * TILESIZE, 7 * TILESIZE), [self.visable_sprites], self.obstacles_sprites)
 
     def run(self):
         # update and draw the game
@@ -43,11 +59,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        self.floor_surf = image.load('graphics/maps/Conveyor_Hell_Floor.png').convert_alpha()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
 
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
--- scripts/player.py
+++ scripts/player.py
@@ -4,7 +4,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
 	def __init__(self, pos, groups, obstacle_sprites):
 		super().__init__(groups)
-		self.image = pygame.image.load('graphics/Clyde.png').convert_alpha()
+		self.image = pygame.image.load('graphics/player_characters/Clyde.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0, -12)
 
